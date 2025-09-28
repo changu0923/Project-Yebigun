031519364507046f54660e96755b304fd5e6bd0f
changu0923 <changu0923@gmail.com>
2025-09-28 17:36:41 +0900

    Added : 사격장 꾸미기
    사격장처럼 움직이는 타겟보드 구현

---
+++ Assets/Scripts/Target/TargetMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    private Animator animator;
+
+    private enum TargetState
+    {
+        Idle,
+        Shooting,
+    }
+
+    private TargetState currentState;
+
+    private bool isMoving;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void MoveFoward()
+    {
+        if (isMoving)
+            return;
+
+        if (currentState == TargetState.Shooting)
+            return;
+
+        isMoving = true;
+        currentState = TargetState.Shooting;        
+        animator.SetTrigger("Foward");
+    }
+
+    public void MoveBackward()
+    {
+        if (isMoving) 
+            return;
+
+        if (currentState == TargetState.Idle)
+            return;
+
+        isMoving = true;
+        currentState = TargetState.Idle;
+        animator.SetTrigger("Backward");
+    }
+
+    #region AnimationEvents
+    public void OnFowardDone()
+    {
+        isMoving = false;
+    }
+
+    public void OnBackwardDone()
+    {
+        isMoving = false;
+    }
+    #endregion
+}
