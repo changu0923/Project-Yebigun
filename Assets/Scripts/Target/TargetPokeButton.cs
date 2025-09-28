cd583cb429d2781b232451cbe658fa59455849b2
changu0923 <changu0923@gmail.com>
2025-09-28 22:11:12 +0900

    Added : 버튼에 소리 추가

--- Assets/Scripts/Target/TargetPokeButton.cs
+++ Assets/Scripts/Target/TargetPokeButton.cs
@@ -1,17 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetPokeButton : MonoBehaviour
 {
     [SerializeField] TargetMovement targetMovement;
+
+    private bool isPushed = false;
+
     public void OnFowardButtonPushed()
     {
         targetMovement.MoveFoward();
+        StartAnimation();
     }
 
     public void OnBackwardButtonPushed()
     {
         targetMovement.MoveBackward();
+        StartAnimation();
+    }
+
+    private void StartAnimation()
+    {
+        if (!isPushed)
+        {
+            StartCoroutine(PlayAnimation());
+        }
+    }
+
+    private IEnumerator PlayAnimation()
+    {
+        isPushed = true;
+
+        Vector3 currentPos = transform.localPosition;
+        Vector3 desiredPos = currentPos - new Vector3(0f, 0.1f, 0f);
+        float duration = 0.33f;
+
+        float t = 0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(currentPos, desiredPos, t / duration); 
+            yield return null;
+        }
+        transform.localPosition = desiredPos;
+
+        t = 0f;
+        while(t<duration)
+        {
+            t += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(desiredPos, currentPos, t / duration);
+            yield return null;
+        }
+        transform.localPosition = currentPos;
+        isPushed = false;
     }
 }
